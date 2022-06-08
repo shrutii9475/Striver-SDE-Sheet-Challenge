8493c120accc44d6c483c0a57a219a29c80e40de
shrutii9475 <shruti.180singh@gmail.com>
2022-06-08 21:11:40 +0530

    Day2

---
+++ DAY2/q2_merge_sorted_array.cpp
@@ -0,0 +1,39 @@
+/***********
+ * https://leetcode.com/problems/merge-sorted-array/submissions/
+ * *******/
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        
+        int i=m-1; 
+        int j=n-1;
+        int k=n+m-1;
+        while(i>=0 && j>=0)
+        {
+            if(n1[i] > n2[j])
+            {
+                n1[k] = n1[i];
+                i--; k--;
+            }
+            else
+            {
+                n1[k] = n2[j];
+                j--; k--;
+            }
+        }
+        while(i>=0)
+        {
+            n1[k] = n1[i];
+                i--; k--;
+        }
+        while(j>=0)
+        {
+            n1[k] = n2[j];
+                j--; k--;
+        }
+    }
+};
